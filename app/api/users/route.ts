b398227587218b849ff2ec06566af8967565f1d7
EDubov <Edubov@project.ibs.ru>
2024-08-23 20:03:01 +0500

    [wip]: Cannot find module '@prisma/client' or its corresponding type declarations

--- app/api/users/route.ts
+++ app/api/users/route.ts
@@ -4,7 +4,6 @@ import { NextRequest, NextResponse } from 'next/server';
 export async function GET() {
   // SELECT * FROM users WHERE email = 'emasd'
   const users = await prisma.user.findMany();
-
   return NextResponse.json(users);
 }
 
