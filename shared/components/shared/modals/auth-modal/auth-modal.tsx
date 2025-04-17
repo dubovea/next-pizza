20e6ee2ac0d6c51b045dc9eade19a4a46d232467
EDubov <Edubov@project.ibs.ru>
2025-04-17 12:59:56 +0500

    fix 21:11:02

--- shared/components/shared/modals/auth-modal/auth-modal.tsx
+++ shared/components/shared/modals/auth-modal/auth-modal.tsx
@@ -1,15 +1,22 @@
 "use client";
 
-import React from "react";
+import React, { useState } from "react";
 import { Dialog, DialogContent } from "@/shared/components/ui/dialog";
 import { Button } from "@/shared/components/ui/button";
 import { signIn } from "next-auth/react";
+import { LoginForm } from "./forms/login-form";
 interface Props {
   open: boolean;
   onClose: () => void;
 }
 
 export const AuthModal: React.FC<Props> = ({ open, onClose }) => {
+  const [type, setType] = useState<"login" | "register">("login");
+
+  const onSwitchType = () => {
+    setType((prev) => (prev === "login" ? "register" : "login"));
+  };
+
   const handleClose = () => {
     onClose();
   };
@@ -17,6 +24,11 @@ export const AuthModal: React.FC<Props> = ({ open, onClose }) => {
   return (
     <Dialog open={open} onOpenChange={handleClose}>
       <DialogContent className="w-[450px] bg-white p-10">
+        {type === "register" ? (
+          <h2 className="text-2xl font-bold">Регистрация</h2>
+        ) : (
+          <LoginForm />
+        )}
         <hr />
         <div className="flex gap-2">
           <Button
@@ -55,6 +67,15 @@ export const AuthModal: React.FC<Props> = ({ open, onClose }) => {
             Google
           </Button>
         </div>
+
+        <Button
+          variant="outline"
+          onClick={onSwitchType}
+          type="button"
+          className="h-12"
+        >
+          {type !== "login" ? "Войти" : "Регистрация"}
+        </Button>
       </DialogContent>
     </Dialog>
   );
